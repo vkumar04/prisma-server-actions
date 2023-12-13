9e5f593448a26c85b55426c2aae4d169af7fb622
Vick Kumar <Vick.Kumar@petscreening.com>
2023-12-13 09:38:56 -0500

    created tanstack provider

---
+++ utils/Providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
